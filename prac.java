2254aad33396214eaecbec8ab2497b4df89c1e89
natalienn <nn268@byu.edu>
2024-01-17 14:22:30 -0700

    init(phase0)

---
+++ prac.java
@@ -0,0 +1,14 @@
+public class SimpleJavaClass{
+    private in x;
+    private static boolean b = true;
+    static{
+        b = true;
+    }
+    public static void main(){
+        System.out.printIn("Hello from SimpleJavaClass");
+    }
+}
+
+// complile javac prac.java
+// java -cp prac.java
+// javac -d 
